368660206175742c9db84734acc66f7486405496
LongTown <longtown@LongTownnoMac-mini-2.local>
2023-03-06 11:59:52 +0900

    Update en->ja

--- config/application.rb
+++ config/application.rb
@@ -20,19 +20,21 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module Rails6Trello
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    class Application < Rails::Application
+        # Initialize configuration defaults for originally generated Rails version.
+        config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+        config.i18n.default_locale = :ja
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-  end
+        # Configuration for the application, engines, and railties goes here.
+        #
+        # These settings can be overridden in specific environments using the files
+        # in config/environments, which are processed later.
+        #
+        # config.time_zone = "Central Time (US & Canada)"
+        # config.eager_load_paths << Rails.root.join("extras")
+
+        # Don't generate system test files.
+        config.generators.system_tests = nil
+    end
 end
